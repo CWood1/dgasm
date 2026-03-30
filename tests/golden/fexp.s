7ccad77d9c209f4e054e528829bcd46adf386101
Connor Wood <connorwood71@gmail.com>
2026-03-30 21:05:12 +0100

    Add all one-acc FPU golden tests

---
+++ tests/golden/fexp.s
@@ -0,0 +1,5 @@
+	// Test: FEXP instruction, with accumulator sweep
+	FEXP 0
+	FEXP 1
+	FEXP 2
+	FEXP 3
---
+++ tests/golden/fscal.s
@@ -0,0 +1,5 @@
+	// Test: FSCAL instruction, with accumulator sweep
+	FSCAL 0
+	FSCAL 1
+	FSCAL 2
+	FSCAL 3
